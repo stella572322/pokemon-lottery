4c11144c74138f846e5e5fc7097ac4e1db362b5b
stella572322 <yuxiangsu0220@gmail.com>
2021-04-21 09:16:07 +0800

    feat: create react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
